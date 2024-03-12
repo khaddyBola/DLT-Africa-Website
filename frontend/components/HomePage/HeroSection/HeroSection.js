7dcc774fa9370a127b3a0c90c93e4f651e21b0ea
DLT-Africa <info@dltafrica.io>
2024-03-12 12:55:41 +0000

    tailwind configured

--- frontend/components/HomePage/HeroSection/HeroSection.js
+++ frontend/components/HomePage/HeroSection/HeroSection.js
@@ -1,16 +1,15 @@
-
 const HeroSection = () => {
   return (
-    <div >
-
+    <div>
       <div className="flex h-[657px]">
-        <div className="w-[548px] bg-blue-200"> Change your career, change your life.</div>
+        <div className="w-[548px] bg-blue-200">
+          {" "}
+          Change your career, change your life.
+        </div>
         <div className="bg-yellow-900 flex-grow"></div>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;

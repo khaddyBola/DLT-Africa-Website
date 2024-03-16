a56c3c0c4e25b0012f59d9334c76218179113db3
DLT-Africa <142330919+DLT-Africa@users.noreply.github.com>
2024-03-16 15:38:50 +0000

    new

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 
@@ -19,6 +20,12 @@ app.use(
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
+
+app.use("/api/cohorts", userRoute);
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
 const PORT = process.env.PORT || 5000;
 

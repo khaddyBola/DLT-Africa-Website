d3e16820f159e18b023f7748910557268ae71077
Smi-lin <olajideolabimpe26@gmail.com>
2024-03-16 11:42:10 +0100

    incubator

--- frontend/components/Event/EventIncubator_CurrentAndUpcoming.js
+++ frontend/components/Event/EventIncubator_CurrentAndUpcoming.js
@@ -1,8 +1,8 @@
 const EventIncubator_CurrentAndUpcoming = () => {
   return (
     <div className="flex w-[1440px] px-281 py-84 justify-center items-center">
-      <h1 className="absolute w-[24px] h-[24px] left-[762px] top-[326px]">Data</h1>
-      
+      <h1 className="">Data</h1>
+      <h3>Learn, build and showcase your skills alongside like-minded peers.</h3>
     </div>
   )
 }

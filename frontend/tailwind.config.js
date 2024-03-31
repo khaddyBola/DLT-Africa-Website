92f50f3882ed35dafc05b89d32f409d0c2457b3a
MatexConcept <matexconcept@gmail.com>
2024-03-31 18:17:31 +0100

    all

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,20 +9,7 @@ module.exports = withMT({
 		// Or if using `src` directory:
 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-  theme: {
-    extend: {
-     
-    
-    },
-
-  },
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+ 
   theme: {},
   plugins: [],
 });

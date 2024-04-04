a2ade0041b1165fbe70722e974b5bb804fd35949
reliable99 <abdulmuhminsodiq@gmail.com>
2024-04-04 14:16:40 +0100

    footer

--- frontend/components/Footer/Footer.js
+++ frontend/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 // import Link from "next/link";
 // import {Link} from 'next/link'
+
 import {
   FaFacebook,
   FaGithub,
@@ -14,12 +15,15 @@ const Footer = () => {
       <div className=" flex flex-row justif-center gap-[50px] mr-[300px] ">
         <div className="w-auto  h-auto min-h-[80px] flex flex-col items-center ml-[80px]">
           <div className="flex flex-col gap-2 mt-[76px]">
-            <a href=""> <img
-              src="https://shorturl.at/oGY13"
-              alt="logo"
-              className="w-[126px] h-[50.797px] ml-5"
-            /></a>
-           
+            <a href="">
+              {" "}
+              <img
+                src="https://shorturl.at/oGY13"
+                alt="logo"
+                className="w-[126px] h-[50.797px] ml-5"
+              />
+            </a>
+
             <p className="text-neutral-black font-poppins text-base font-normal leading-[120%] w-[211px] h-[19px] text-left">
               Innovating Through web3...
             </p>
@@ -47,12 +51,16 @@ const Footer = () => {
             <p className="mb-[21px]">
               For more info about our Classes;
               <br />
-               <a href="#"><b> (234)815 650 9701</b></a>
+              <a href="#">
+                <b> (234)815 650 9701</b>
+              </a>
             </p>
             <p>
               Email:
               <br />
-               <a href="#"><b>info@dltafrica.io</b></a>
+              <a href="#">
+                <b>info@dltafrica.io</b>
+              </a>
             </p>
           </div>
           <div className="w-[145px] h-auto min-h-[165px] ml-[50px]">
@@ -75,11 +83,21 @@ const Footer = () => {
       </div>
 
       <div className="flex gap-6 pb-5 justify-center mt-[80px] ">
-        <a href="#"><FaFacebook className="w-[35px] h-[35px]" /></a>
-        <a href="#"><FaInstagram className="w-[35px] h-[35px]" /></a>
-        <a href="#"><FaLinkedin className="w-[35px] h-[35px]" /></a>
-        <a href="#"><FaTwitter className="w-[35px] h-[35px]" /></a>
-        <a href="#"><FaGithub className="w-[35px] h-[35px]" /></a>
+        <a href="#">
+          <FaFacebook className="w-[35px] h-[35px]" />
+        </a>
+        <a href="#">
+          <FaInstagram className="w-[35px] h-[35px]" />
+        </a>
+        <a href="#">
+          <FaLinkedin className="w-[35px] h-[35px]" />
+        </a>
+        <a href="#">
+          <FaTwitter className="w-[35px] h-[35px]" />
+        </a>
+        <a href="#">
+          <FaGithub className="w-[35px] h-[35px]" />
+        </a>
       </div>
     </footer>
   );

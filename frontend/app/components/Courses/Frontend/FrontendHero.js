110e839d117c3f3031351c14f2bd9ddbb235cd09
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-04-08 16:06:45 +0100

    update

--- frontend/app/components/Courses/Frontend/FrontendHero.js
+++ frontend/app/components/Courses/Frontend/FrontendHero.js
@@ -4,7 +4,7 @@ import CourseSection from "../CoursesHero/CourseHeroSec";
 const sectionData = [
   {
     id: 2,
-    title: "Front-End Web Development",
+    title: "Front End Web Development",
     text: "Discover the art of web development with our program. Dive into HTML, CSS, JavaScript, and create stunning sites. Real-world projects and expert guidance await you. Start your journey today!",
     headline1: "Graduate in six months, part-time, and start working in the high-demand field of software development.",
     headline2: "Meet regularly with an experienced mentor to ask the questions you care about.",
